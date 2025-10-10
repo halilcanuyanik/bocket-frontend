9bc56de883797cd72e25b0ea1f80f6a958dd6ee2
Halil Can Uyanık <101755107+halilcanuyanik@users.noreply.github.com>
2025-10-10 16:35:35 +0300

    implement modern routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import AppRouter from "./routes/AppRouter";
 
 function App() {
-  return (
-    <div className="w-[100vw] h-[100vh]">
-      <AppRouter />
-    </div>
-  );
+  return <div className="w-[100vw] h-[100vh]"></div>;
 }
 
 export default App;
---
+++ src/router/router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "*",
+    element: <div>404 No page found!</div>,
+  },
+]);
