d1d8ead32d33553542d10b41ecd54a190724cf8f
Halil Can Uyanık <101755107+halilcanuyanik@users.noreply.github.com>
2025-12-10 16:25:52 +0300

    implemented show pages

--- src/features/show/pages/ShowsPage.jsx
+++ src/features/show/pages/ShowsPage.jsx
@@ -1,5 +1,5 @@
 // REACT HOOKS
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // COMPONENTS
 import Search from '@/components/common/Search';
@@ -26,6 +26,57 @@ function ShowsPage() {
             Add Show
           </button>
         </div>
+      </div>
+
+      <div className="mt-8 w-full flex-1 flex flex-wrap space-x-4 space-y-4">
+        {shows.map((s) => {
+          return (
+            <div className="group w-64 h-72 rounded-sm shadow-md overflow-hidden relative cursor-pointer border border-gray-400">
+              <div className="absolute w-full h-full transition duration-300">
+                <div className="absolute w-full h-full bg-gradient-to-b from-black/60 via-transparent to-black/60 z-1"></div>
+                <img
+                  src={s.coverImage}
+                  className="absolute w-full h-full object-cover transition-all duration-300 group-hover:blur-sm"
+                />
+              </div>
+
+              <div className="absolute inset-0 flex flex-col justify-between p-4 z-3">
+                <div>
+                  <span className="font-bold text-white text-sm block">
+                    {s.title}
+                  </span>
+
+                  <span
+                    className="
+          font-semibold text-white text-sm block line-clamp-2 mt-1
+          opacity-0
+          transition-opacity duration-300
+          group-hover:opacity-100
+        "
+                  >
+                    {s.description}
+                  </span>
+                </div>
+
+                <div className="flex gap-2 overflow-hidden">
+                  {s.performers.map((p) => (
+                    <img
+                      key={p.id}
+                      src={p.avatarImage}
+                      className="w-12 h-12 shadow-md bg-sky-blue p-[2px] object-cover rounded-full"
+                    />
+                  ))}
+
+                  {s.performers.length > 3 && (
+                    <div className="w-12 h-12 bg-gray-700 text-white text-xs flex items-center justify-center rounded-full flex-shrink-0">
+                      +{s.performers.length - 3}
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
 
       {addModalOpen && (
