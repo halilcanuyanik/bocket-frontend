f8e37d55a74b4b277bcc68a82b394bebd87edea5
Halil Can Uyanık <101755107+halilcanuyanik@users.noreply.github.com>
2025-12-10 16:47:33 +0300

    Update ShowsPage.jsx

--- src/features/show/pages/ShowsPage.jsx
+++ src/features/show/pages/ShowsPage.jsx
@@ -1,5 +1,5 @@
 // REACT HOOKS
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 // COMPONENTS
 import Search from '@/components/common/Search';
@@ -39,8 +39,7 @@ function ShowsPage() {
                   className="absolute w-full h-full object-cover transition-all duration-300 group-hover:blur-sm"
                 />
               </div>
-
-              <div className="absolute inset-0 flex flex-col justify-between p-4 z-3">
+              <div className="absolute inset-0 flex flex-col justify-between p-4 z-2">
                 <div>
                   <span className="font-bold text-white text-sm block">
                     {s.title}
@@ -49,8 +48,7 @@ function ShowsPage() {
                   <span
                     className="
           font-semibold text-white text-sm block line-clamp-2 mt-1
-          opacity-0
-          transition-opacity duration-300
+          opacity-0 transition-opacity duration-300
           group-hover:opacity-100
         "
                   >
@@ -58,20 +56,38 @@ function ShowsPage() {
                   </span>
                 </div>
 
-                <div className="flex gap-2 overflow-hidden">
-                  {s.performers.map((p) => (
+                <div className="flex items-center overflow-hidden relative">
+                  {s.performers.slice(0, 3).map((p, index) => (
                     <img
                       key={p.id}
                       src={p.avatarImage}
-                      className="w-12 h-12 shadow-md bg-sky-blue p-[2px] object-cover rounded-full"
+                      className={`
+        w-12 h-12 object-cover rounded-full border-2 border-white
+        ${index !== 0 ? '-ml-4' : ''} shadow-md
+      `}
                     />
                   ))}
 
                   {s.performers.length > 3 && (
-                    <div className="w-12 h-12 bg-gray-700 text-white text-xs flex items-center justify-center rounded-full flex-shrink-0">
+                    <div className="-ml-4 w-12 h-12 bg-black text-white text-xs flex items-center justify-center rounded-full border-2 border-white flex-shrink-0">
                       +{s.performers.length - 3}
                     </div>
                   )}
+
+                  <div
+                    className="
+      absolute bottom-2 right-2 flex gap-2 opacity-0 pointer-events-none
+      transition-opacity duration-300
+      group-hover:opacity-100 group-hover:pointer-events-auto
+    "
+                  >
+                    <button className="px-2 py-1 text-xs bg-blue-700 text-white border-gray-400 rounded hover:bg-blue-800 shadow-md cursor-pointer">
+                      Edit
+                    </button>
+                    <button className="px-2 py-1 text-xs bg-red-700 text-white border-gray-400 rounded hover:bg-red-800 shadow-md cursor-pointer">
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
